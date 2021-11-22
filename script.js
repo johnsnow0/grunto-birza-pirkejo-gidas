0274d734aa6540b671444e501983331e410ae530
Mindaugas Vilkas <46817295+johnsnow0@users.noreply.github.com>
2021-11-22 13:30:04 +0200

    Sutvarkyta navigacija tarp meniu lygiu
    dirba visa navigacija tarp visu lygiu

--- script.js
+++ script.js
@@ -12,6 +12,20 @@ function daryk(reiksme) {
     var kat11s7 = document.getElementById("s7")
     var kat11s8 = document.getElementById("s8")
 
+    var kat21g1 = document.getElementById("g1")
+    var kat21g2 = document.getElementById("g2")
+    var kat21g3 = document.getElementById("g3")
+    var kat21g4 = document.getElementById("g4")
+    var kat21g5 = document.getElementById("g5")
+    var kat21g6 = document.getElementById("g6")
+    var kat21g7 = document.getElementById("g7")
+    var kat21g8 = document.getElementById("g8")
+
+    var kat31k1 = document.getElementById("k1")
+    var kat31k2 = document.getElementById("k2")
+    var kat31k3 = document.getElementById("k3")
+    
+
     if (reiksme == 0) {
         kat1.style.display = "flex"
         kat11.style.display = "none"
@@ -19,6 +33,27 @@ function daryk(reiksme) {
         kat31.style.display = "none"
         kat11s1.style.display = "none"
         kat11s2.style.display = "none"
+        kat11s3.style.display = "none"
+        kat11s4.style.display = "none"
+        kat11s5.style.display = "none"
+        kat11s6.style.display = "none"
+        kat11s7.style.display = "none"
+        kat11s8.style.display = "none"
+
+        kat21g1.style.display = "none"
+        kat21g2.style.display = "none"
+        kat21g3.style.display = "none"
+        kat21g4.style.display = "none"
+        kat21g5.style.display = "none"
+        kat21g6.style.display = "none"
+        kat21g7.style.display = "none"
+        kat21g8.style.display = "none"
+
+        kat31k1.style.display = "none"
+        kat31k2.style.display = "none"
+        kat31k3.style.display = "none"
+        
+
     }
     if (reiksme == 1) {
         kat1.style.display = "none"
@@ -57,14 +92,154 @@ function daryk(reiksme) {
         kat11.style.display = "none"
         kat21.style.display = "none" 
         kat31.style.display = "none"
-        kat11s1.style.display = "flex"
+        kat11s3.style.display = "flex"
     }
     if (reiksme == 14){
         kat1.style.display = "none"
         kat11.style.display = "none"
         kat21.style.display = "none" 
         kat31.style.display = "none"
-        kat11s1.style.display = "flex"
+        kat11s4.style.display = "flex"
+    }
+    if (reiksme == 15){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat11s5.style.display = "flex"
+    }
+    if (reiksme == 16){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat11s6.style.display = "flex"
+    }
+    if (reiksme == 17){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat11s7.style.display = "flex"
+    }
+    if (reiksme == 18){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat11s8.style.display = "flex"
+    }
+    if (reiksme == 21){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g1.style.display = "flex"
+    }
+    if (reiksme == 22){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g2.style.display = "flex"
+    }
+    if (reiksme == 23){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g3.style.display = "flex"
+    }
+    if (reiksme == 24){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g4.style.display = "flex"
+    }
+    if (reiksme == 25){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g5.style.display = "flex"
+    }
+    if (reiksme == 26){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g6.style.display = "flex"
+    }
+    if (reiksme == 27){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g7.style.display = "flex"
+    }
+    if (reiksme == 28){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat21g8.style.display = "flex"
+    }
+    if (reiksme == 31){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k1.style.display = "flex"
+    }
+    if (reiksme == 32){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k2.style.display = "flex"
+    }
+    if (reiksme == 33){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k3.style.display = "flex"
+    }
+    if (reiksme == 34){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k4.style.display = "flex"
+    }
+    if (reiksme == 35){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k5.style.display = "flex"
+    }
+    if (reiksme == 36){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k6.style.display = "flex"
+    }
+    if (reiksme == 37){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k7.style.display = "flex"
+    }
+    if (reiksme == 38){
+        kat1.style.display = "none"
+        kat11.style.display = "none"
+        kat21.style.display = "none" 
+        kat31.style.display = "none"
+        kat31k8.style.display = "flex"
     }
 
 }
